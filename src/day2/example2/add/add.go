132306aff676a3426fc17ff6e43396e96df045fe
HSM <Haishenmingx@gmail.com>
2017-08-09 21:49:07 +0800

    day2

---
+++ src/day2/example2/add/add.go
@@ -0,0 +1,10 @@
+package add
+
+var Name string
+var Age int
+
+
+func init(){
+	Name = "Leo"
+	Age = 22
+}
---
+++ src/day2/example2/main/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	a "day2/example2/add"
+	_ "day2/example2/test"
+	"fmt"
+)
+
+
+func main() {
+	fmt.Println(a.Name)
+	fmt.Println(a.Age)
+}
